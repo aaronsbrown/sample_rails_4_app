91934ba3c6907a2d65885561ed79a69103f4c7a8
Aaron Brown <aaron@stitchfix.com>
2013-12-13 22:12:59 -0800

    finish feature

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,19 +2,46 @@ require 'spec_helper'
 
 feature "StaticPages" do
   
-  scenario "Home page should have the content 'Sample App'" do
-    visit '/static_pages/home'
-    expect(page).to have_content('Sample App')
+  context "Home Page" do
+
+    scenario "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    scenario "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+
   end
 
-  scenario "Help Page should have the content 'Help'" do
-    visit '/static_pages/help'
-    expect(page).to have_content('Help')
+  context "Help Page" do
+
+    scenario "Help Page should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    scenario "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+
   end
 
-  scenario "About page should have the content 'About Us'" do
-    visit '/static_pages/about'
-    expect(page).to have_content('About Us')
+  context "About Page" do
+
+    scenario "About page should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    scenario "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    end
+
   end
 
 end
