22e7ec442cc7d1d8347cc69761cb233ab5c37899
Aaron Brown <aaron@stitchfix.com>
2013-12-15 09:58:11 -0800

    DRY up specs with shared examples; default title value

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -6,63 +6,48 @@ feature "Static pages" do
 
   given(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
+  shared_examples_for "all static pages" do
+    scenario { should have_selector('h1', text: heading) }
+    scenario { should have_title(full_title(page_title)) }
+  end
+
   context "Home Page" do
     before { visit root_path }
 
-    scenario "should have the content 'Sample App'" do
-      should have_content('Sample App')
-    end
-
-    scenario "should have base title" do
-      should have_title("#{base_title}")
-    end
-
-    scenario "should not have a custom page title" do
-      should_not have_title("| Home")
-    end
-
+    let(:heading) { 'Sample App'}
+    let(:page_title) { '' }
+    
+    it_should_behave_like "all static pages"
+    scenario { should_not have_title("| Home") }
   end
 
   context "Help Page" do
 
-    scenario "Help Page should have the content 'Help'" do
-      visit help_path
-      should have_content('Help')
-    end
+    before { visit help_path }
 
-    scenario "should have the right title" do
-      visit help_path
-      should have_title("#{base_title} | Help")
-    end
+    let(:heading) { 'Help'}
+    let(:page_title) { 'Help' }
 
+    it_should_behave_like "all static pages"
   end
 
   context "About Page" do
 
-    scenario "About page should have the content 'About Us'" do
-      visit about_path
-      should have_content('About Us')
-    end
+    before { visit about_path }
 
-    scenario "should have the right title" do
-      visit about_path
-      should have_title("#{base_title} | About")
-    end
-
+    let(:heading) { 'About Us'}
+    let(:page_title) { 'About' } 
+ 
+    it_should_behave_like "all static pages"
   end
 
   context "Contact Page" do
 
-    scenario "Contact page should have the content 'Contact Ruby on Rails'" do
-      visit contact_path
-      should have_content('Contact Ruby on Rails')
-    end
-
-    scenario "should have the right title" do
-      visit contact_path
-      should have_title("#{base_title} | Contact")
-    end
-
+    before { visit contact_path }
+    let(:heading) { 'Contact'}
+    let(:page_title) { 'Contact'}
+ 
+    it_should_behave_like "all static pages"
   end
 
 end
