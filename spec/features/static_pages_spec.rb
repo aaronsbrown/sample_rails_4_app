41e37da4defbef40aac707d8ff6c06cb069a49f4
Aaron Brown <aaron@stitchfix.com>
2013-12-14 16:27:34 -0800

    no home title; title helper

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -11,9 +11,14 @@ feature "StaticPages" do
       expect(page).to have_content('Sample App')
     end
 
-    scenario "should have the right title" do
+    scenario "should have basetitle" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}Home")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    scenario "should not have a custom pate title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title("| Home")
     end
 
   end
