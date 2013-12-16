7a7fad3ec49a86f075135d652419fb351c02861e
Aaron Brown <aaron@stitchfix.com>
2013-12-15 19:48:08 -0800

    user resource, basic user view page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ SampleApp::Application.routes.draw do
   get '/about',   to: "static_pages#about"
   get '/contact', to: "static_pages#contact"
 
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
