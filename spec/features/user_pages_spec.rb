9b007b9ef29e5be720a60f0508338b26c48bd6aa
Aaron Brown <aaron@stitchfix.com>
2013-12-21 18:02:33 -0800

    style form; invalid user creation use case

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -9,18 +9,33 @@ feature "User pages" do
   	before { visit signup_path }
 
 	  scenario "should have content" do
-	  	should have_content('Sign up')
+	  	page.should have_content('Sign up')
 	  end
 
 	  scenario "should have title" do
-	  	should have_title(full_title('Sign up'))
+	  	page.should have_title(full_title('Sign up'))
 	  end
+
+	  context "should support user registration"  do
+	  	let(:submit) { "Create My Account" }
+	  	scenario "with invalid information" do 
+	  		expect { click_button submit }.not_to change(User, :count)
+	  	end
+
+	  	# scenario "with valid information" do 
+	  	# 	fill_in "Name", with: "Example User"
+	  	# 	fill_in "Email", with: "user@xample.com"
+	  	# 	fill_in "Password", with: "password"
+	  	# 	fill_in "Password Confirmation", with: "password"
+	  	# 	expect { click_button submit }.to change(User, :count).by(1)
+	  	# end
+		end
 	end
 
 	context "Profile Page" do
 		let(:user) { FactoryGirl.create(:user) }
 		before { visit user_path(user) }
-		it { should have_content(user.name) }
-		it { should have_title(user.name) }
+		scenario { page.should have_content(user.name) }
+		scenario { page.should have_title(user.name) }
 	end
 end
