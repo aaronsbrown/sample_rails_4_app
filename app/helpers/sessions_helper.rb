37405f9f6f5988351968fb71cfdd4310854d8475
Aaron Brown <aaron@stitchfix.com>
2013-12-24 20:46:57 -0800

    custom matcher; sign out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,7 +18,9 @@ module SessionsHelper
 	end
 
 	def sign_out(user)
-		cookies.permanent[:remember_token] = nil
+		current_user.update_attribute(:remember_token, 
+								 User.encrypt(User.new_remember_token))
+		cookies.delete(:remember_token)
 		self.current_user = nil
 	end
 
--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -11,28 +11,33 @@ feature 'Authentication Pages' do
 		context "with invalid information" do
 			before { click_button "Sign In" }
 			scenario { page.should have_title(full_title('Sign In'))}
-			scenario { page.should have_selector('.alert-danger', text: 'Invalid') }
+			scenario { page.should have_error_message('Invalid') }
 
 			context "after visiting another page" do
 				before { click_link "Home" }
 				scenario { page.should_not have_selector('.alert-danger') }
+				# TODOAB: couldn't get this to work
+				# scenario { page.should_not have_error_message('Invalid') }
 			end
 		end
 		
 		context "with valid information" do 
 
-		let(:user) { FactoryGirl.create(:user) }
-		before do
-			fill_in "Email", with: user.email.upcase
-			fill_in "Password", with: user.password
-		  click_button "Sign In" 
-		end
+			let(:user) { FactoryGirl.create(:user) }
+			before do
+				valid_signin(user)
+			end
 
-		scenario { page.should have_title(full_title(user.name)) }
-		scenario { page.should have_link('Profile', href: user_path(user)) }
-		scenario { page.should have_link('Settings') }
-		scenario { page.should have_link('Sign Out', href: signout_path) }
-		scenario { page.should_not have_link('Sign In', href: signin_path) }
+			scenario { page.should have_title(full_title(user.name)) }
+			scenario { page.should have_link('Profile', href: user_path(user)) }
+			scenario { page.should have_link('Settings') }
+			scenario { page.should have_link('Sign Out', href: signout_path) }
+			scenario { page.should_not have_link('Sign In', href: signin_path) }
+
+			describe "followed by signout" do
+				before { click_link 'Sign Out' }
+				scenario { page.should have_link('Sign In') }
+			end
 
 		end
 
