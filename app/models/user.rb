0d966c5aa2ded422afa26aec832526adaef8c3d9
Aaron Brown <aaron@stitchfix.com>
2013-12-30 12:23:40 -0800

    micro post model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
+	has_many :microposts
+
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: {with: VALID_EMAIL_REGEX }
 	validates :password, length: { minimum: 6 }
