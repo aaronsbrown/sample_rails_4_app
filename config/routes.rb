6e107619ea1070bbd11a19cc172592317915fff6
Aaron Brown <aaron@stitchfix.com>
2013-12-30 13:25:31 -0800

    show micro posts; micro posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ SampleApp::Application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
+  resources :microposts, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
