6bef7cdba028503445b2ad0a745d024d897cddd8
Aaron Brown <aaron@stitchfix.com>
2013-12-30 15:01:55 -0800

    create micropost

---
+++ spec/features/micropost_pages_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+feature "Micropost" do
+
+	subject { page }
+
+	let(:user) { FactoryGirl.create(:user) }
+	before { signin user }
+
+	context "micropost creation" do
+		
+		before { visit root_path }
+		context "with invalid information" do
+
+			scenario "should not create a micropost" do
+				expect { click_button "Post" }.not_to change(Micropost, :count)
+			end
+
+			context "error messages" do
+				before { click_button "Post" }
+				scenario { page.should have_content('error') }
+			end
+
+		end
+		context "with valid information" do
+
+			before { fill_in 'micropost_content', with: 'Lorem Ipsum' }
+			scenario "should create a micropost" do
+				expect { click_button "Post" }.to change(Micropost, :count).by(1)
+			end
+			
+		end
+
+	end
+	
+
+end
